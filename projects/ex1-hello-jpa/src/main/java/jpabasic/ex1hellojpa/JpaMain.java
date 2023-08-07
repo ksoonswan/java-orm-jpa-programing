08fbed1cf8fba28be6837698b8f6904c4a4f425e
soonswan <swan.kim@oysterable.com>
2023-08-07 14:26:26 +0900

    [mod] JPA 준영속상태

--- projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
+++ projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
@@ -17,13 +17,14 @@ public class JpaMain {
 
     try {
       // 영속
-      Member member = new Member(200L, "member200");
-      em.persist(member);
+      Member member1 = em.find(Member.class, 150L);
+      member1.setName("AAAAA");
 
-      em.flush();
+      em.clear();
+
+      Member member2 = em.find(Member.class, 150L);
 
       System.out.println("==========================");
-
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
