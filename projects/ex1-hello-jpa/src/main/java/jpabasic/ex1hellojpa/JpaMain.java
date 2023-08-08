4867492d03cad1b441b2368075301fa0a852a1bf
soonswan <swan.kim@oysterable.com>
2023-08-08 14:27:56 +0900

    [add]기본키 매핑

--- projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
+++ projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
@@ -18,9 +18,7 @@ public class JpaMain {
     try {
 
       Member member = new Member();
-      member.setId(3L);
-      member.setUsername("C");
-      member.setRoleType(RoleType.USER);
+      member.setUsername("AAAA");
 
       em.persist(member);
 
--- projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/Member.java
+++ projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/Member.java
@@ -1,39 +1,28 @@
 package jpabasic.ex1hellojpa;
 
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.TableGenerator;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Entity
+@TableGenerator(
+    name = "MEMBER_SEQ_GENERATOR",
+    table = "MY_SEQUENCES",
+    pkColumnName = "MEMBER_SEQ", allocationSize = 1
+)
 public class Member {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column(name = "name")
   private String username;
-
-  private Integer age;
-
-  @Enumerated(EnumType.STRING)
-  private RoleType roleType;
-
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date createdDate;
-
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date lastModifiedDate;
-
-  @Lob
-  private String description;
 }
