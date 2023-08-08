58f81f2fd063bc338ab011bee3516aecc03eef03
soonswan <swan.kim@oysterable.com>
2023-08-08 18:58:44 +0900

    [add] 양방향 매핑을 할때는 양방향 모두 값을 세팅해주는 것이 중요하다=> entity에 함수만들어 사용

--- projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
+++ projects/ex1-hello-jpa/src/main/java/jpabasic/ex1hellojpa/JpaMain.java
@@ -21,26 +21,29 @@ public class JpaMain {
       // 저장
       Team team = new Team();
       team.setName("TeamA");
+      // 역방향(주인이 아닌 방향)만 연관관계 설정
+//      team.getMembers().add(member);
       em.persist(team);
 
       Member member = new Member();
       member.setUserName("member1");
-      member.setTeam(team);
+//      member.changeTeam(team);
       em.persist(member);
 
-      em.flush();
-      em.clear();
+      team.addMember(member);
 
-      Member findMember = em.find(Member.class, member.getId());
-      List<Member> members = findMember.getTeam().getMembers();
+      // 결론은 양방향 매핑을 할때는 양방향 모두 값을 세팅해주는 것이 중요하다.
+//      team.getMembers().add(member);
 
-      for (Member mbr : members) {
-        System.out.println("Member: " + mbr.getUserName());
+//      em.flush();
+//      em.clear();
+
+      Team findTeam = em.find(Team.class, team.getId());
+      List<Member> members = findTeam.getMembers();
+
+      for (Member m : members) {
+        System.out.println("Member = " + m.getUserName());
       }
-
-      Team findTeam = findMember.getTeam();
-
-      System.out.println("findTeam = " + findTeam.getName());
 
       tx.commit();
     } catch (Exception e) {
