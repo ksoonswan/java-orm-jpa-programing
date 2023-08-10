7083031f762fd4de7c875b39a6fcb163bdebcb15
soonswan <swan.kim@oysterable.com>
2023-08-10 19:15:25 +0900

    [add] JPQL 쿼리 기본 문법

--- projects/jpql/src/main/java/jpql/JpaMain.java
+++ projects/jpql/src/main/java/jpql/JpaMain.java
@@ -4,8 +4,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
+import java.util.List;
 import jpql.entity.Member;
 
 public class JpaMain {
@@ -25,9 +25,17 @@ public class JpaMain {
       member.setAge(100);
       em.persist(member);
 
-      TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-      TypedQuery<String> query2 = em.createQuery("select m.userName from Member m", String.class);
-      Query query3 = em.createQuery("select m.userName, m.age from Member m");
+      TypedQuery<Member> query = em.createQuery("select m from Member m WHERE m.userName = :userName", Member.class);
+      query.setParameter("userName", "member1");
+      List<Member> resultList = query.getResultList();
+
+      for (Member member1 : resultList) {
+        System.out.println("member: " + member1.getUserName());
+      }
+
+      // 이거는 데이터가 없거나, 두개 이상있으면 오류가 나기 때문에 권장하지 않는다.
+      Member resultOne = query.getSingleResult();
+      System.out.println("Single Member: " + resultOne.getUserName());
 
       tx.commit();
     } catch (Exception e) {
