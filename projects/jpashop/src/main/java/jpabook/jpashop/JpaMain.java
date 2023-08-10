2e3187156316080e6c13c16b6cddd5e4a27bc2e7
soonswan <swan.kim@oysterable.com>
2023-08-10 16:07:27 +0900

    [add] Critaria는 실무에서는 권장하지 않음, sql 보다 더 가독성도 떨어지고 힘듬

--- projects/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ projects/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import java.util.List;
 import jpabook.jpashop.domain.Member;
 
@@ -19,10 +22,25 @@ public class JpaMain {
     tx.begin();
 
     try {
+//      Member newMember = new Member();
+//      newMember.setName("kim");
+//      em.persist(newMember);
 
-      List resultList = em.createQuery("select m from Member m where m.name like '%kim%'",
-              Member.class)
+      CriteriaBuilder cb = em.getCriteriaBuilder();
+      CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+      Root<Member> m = query.from(Member.class);
+
+      CriteriaQuery<Member> cq = query.select(m)
+          .where(cb.equal(m.get("name"), "kim"));
+
+      List<Member> resultList = em.createQuery(cq)
           .getResultList();
+
+      for (Member member : resultList) {
+        System.out.println("=============");
+        System.out.println(member.getName());
+      }
 
       tx.commit();
     } catch (Exception e) {
